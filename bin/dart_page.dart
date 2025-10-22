c6a82092859625432c3e75a93ae6da3a5bda66ca
Huzaifa sani <huzaifa sani>
2025-10-22 23:28:52 +0600

    first commit

---
+++ bin/dart_page.dart
@@ -0,0 +1,50 @@
+// dart start
+void main (){
+
+  // print
+  print("Huzaifa sani");
+
+  String name = "joy";
+  int number = 45;
+  double cgpa= 4.25;
+  bool right = true;
+  var myvar = "This is var value";
+  dynamic mydynamic = "This is dynamic valu";
+  mydynamic = 25;
+  mydynamic = 25.4;
+
+  const myconst = "This is Const data";
+  final myfinal = 125;
+
+
+  print(name);
+  print(number);
+  print(cgpa);
+  print(name);
+  print(right);
+  print(myvar);
+  print(mydynamic);
+  print(myconst);
+  print(myfinal);
+
+  print("${name}");
+
+  print(name.isEmpty);
+  print(name.length);
+  print(name.toLowerCase());
+  print(name.toUpperCase());
+  print(number.toString());
+  print(number.runtimeType);
+  print(number.isEven);
+  print(number.isNegative);
+
+  // single line comment
+  /*
+
+  This is multiline comment
+
+   */
+
+
+
+}
